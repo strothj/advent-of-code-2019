32ca37784571612fb195497ba8eb2ff008c572a1
strothj <jason@jasons.io>
2019-12-03 06:44:37 -0500

    feat: add day 2 part 2 solution

--- day_02.go
+++ day_02.go
@@ -5,37 +5,87 @@ import (
 	"strings"
 )
 
-func day02(input string, programAlarm bool) int {
+func day02(input string, programAlarm bool, nounVerbSearch bool) int {
 	inputValues := strings.Split(input, ",")
-	memory := make([]int, 0, len(inputValues))
-	for _, v := range inputValues {
-		memoryValue, _ := strconv.Atoi(v)
-		memory = append(memory, memoryValue)
-	}
-	if programAlarm {
-		memory[1] = 12
-		memory[2] = 2
+	var memory []int
+
+	loadProgram := func(noun, verb int) {
+		memory = make([]int, 0, len(inputValues))
+		for _, v := range inputValues {
+			memoryValue, _ := strconv.Atoi(v)
+			memory = append(memory, memoryValue)
+		}
+		if noun != -1 {
+			memory[1] = noun
+		}
+		if verb != -1 {
+			memory[2] = verb
+		}
 	}
 
-	pc := 0
-	exit := false
-	for {
-		switch memory[pc] {
-		case 1:
-			memory[memory[pc+3]] = memory[memory[pc+1]] + memory[memory[pc+2]]
-			break
-		case 2:
-			memory[memory[pc+3]] = memory[memory[pc+1]] * memory[memory[pc+2]]
-			break
-		case 99:
-			exit = true
-			break
+	executeProgram := func() int {
+		for instructionPointer := 0; instructionPointer < len(memory); instructionPointer += 4 {
+			opCode := memory[instructionPointer]
+			parameter0Address := 0
+			parameter1Address := 0
+			resultAddress := 0
+
+			switch opCode {
+			case 1:
+				fallthrough
+			case 2:
+				if instructionPointer+3 >= len(memory) {
+					return -1
+				}
+				parameter0Address = memory[instructionPointer+1]
+				parameter1Address = memory[instructionPointer+2]
+				resultAddress = memory[instructionPointer+3]
+				break
+			case 99:
+				return memory[0]
+			default:
+				return -2
+			}
+
+			if parameter0Address >= len(memory) || parameter1Address >= len(memory) || resultAddress >= len(memory) {
+				return -3
+			}
+
+			switch opCode {
+			case 1:
+				memory[resultAddress] = memory[parameter0Address] + memory[parameter1Address]
+				break
+			case 2:
+				memory[resultAddress] = memory[parameter0Address] * memory[parameter1Address]
+				break
+			}
 		}
-		if exit {
-			break
-		}
-		pc += 4
+
+		return -1
 	}
 
-	return memory[0]
+	for j := 0; j <= 99; j++ {
+		for k := 0; k <= 99; k++ {
+			noun := -1
+			verb := -1
+			if programAlarm {
+				noun = 12
+				verb = 2
+			}
+			if nounVerbSearch {
+				noun = j
+				verb = k
+			}
+			loadProgram(noun, verb)
+			result := executeProgram()
+			if !nounVerbSearch {
+				return result
+			}
+			if result == 19690720 {
+				return 100*noun + verb
+			}
+		}
+	}
+
+	return -1
 }
